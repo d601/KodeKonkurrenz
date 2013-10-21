43c1217280fbf731cf08c75d03f637b7468f7619
Paul Sladek <pwbsladekcw@gmail.com>
2013-10-21 00:40:10 -0700

    bootstrap has been added

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ KodeKonkurrenz::Application.routes.draw do
   # get "pages/home"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
+  
+  resources :posts
 
   # You can have the root of your site routed with "root"
   root to: "pages#home"
