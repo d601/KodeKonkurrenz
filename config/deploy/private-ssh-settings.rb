32b2fd35ac8026d48c212f91dfc9f8d7beb790b6
d601 <shmeep@gmail.com>
2013-10-23 14:32:42 -0700

    Re-add private SSH settings to .gitignore and remove the file from the tree (D'oh)

--- config/deploy/private-ssh-settings.rb
+++
@@ -1,6 +0,0 @@
-module KodeKonkurrenz-private-ssh-settings
-  user = 'jesse'
-  keyfileLocation = %w(/home/js/.ssh/jesse-key-1.pem)
-  deploymentLocation = '/var/www/testing-jesse'
-  deploymentBranch = 'jesse'
-end
