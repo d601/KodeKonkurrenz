7b31138afbf41edaee157fbd8a42913bce41b32b
Paul Sladek <pwbsladekcw@gmail.com>
2013-10-23 23:50:30 -0700

    private-ssh set, mysql test

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20131024063148) do
+
+  create_table "high_scores", force: true do |t|
+    t.string   "game"
+    t.integer  "score"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "student", id: false, force: true do |t|
+    t.string "id",   limit: 2
+    t.string "name", limit: 20
+    t.string "rank", limit: 2
+  end
 
 end
