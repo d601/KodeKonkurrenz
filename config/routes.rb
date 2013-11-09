1dd9806bb5fb0ec746551752b0f21a6d86395eed
Paul Sladek <pwbsladekcw@gmail.com>
2013-11-09 13:29:53 -0800

    added route for liveGraph. Modified header.

--- config/routes.rb
+++ config/routes.rb
@@ -3,11 +3,12 @@ KodeKonkurrenz::Application.routes.draw do
   # get "pages/home"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
-  
-  resources :posts
 
   # You can have the root of your site routed with "root"
   root to: "pages#home"
+
+  get '/home' => 'pages#home'
+  get '/liveGraph' => 'pages#liveGraph'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
