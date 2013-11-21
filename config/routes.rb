ff12309c7a2ba0bb6fa01f572007c04513ef3eab
d601 <shmeep@gmail.com>
2013-11-20 18:12:50 -0800

    Revert last commit - no need for an entire admin controller.
    This reverts commit c4c2fd1561f402fcbb41cb0eec12cf6973e28182.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 KodeKonkurrenz::Application.routes.draw do
+
   # This line mounts Forem's routes at /forums by default.
   # This means, any requests to the /forums URL of your application will go to Forem::ForumsController#index.
   # If you would like to change where this extension is mounted, simply change the :at option to something different.
@@ -31,7 +32,7 @@ KodeKonkurrenz::Application.routes.draw do
   match '/termsOfService' , to: 'pages#termsOfService', via: 'get'
   match '/competition', to: 'pages#competition', via: 'get'
 
-  match '/admin_panel', to: 'admin_panel#index', via: 'get'
+
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
